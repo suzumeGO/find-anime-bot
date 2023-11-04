2e71fd69b7f879dd69b50c27313ce66e21f79a8f
Артем Зотов <zotov.artem.2019@gmail.com>
2023-11-04 18:45:17 +0300

    initial commit

---
+++ src/main/java/com/example/findanime/FindAnimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.findanime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindAnimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/findanime/FindAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.findanime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindAnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
